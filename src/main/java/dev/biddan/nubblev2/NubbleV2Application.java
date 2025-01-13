db396f166abcee59af828462acc3b4d6710848fa
biddan606 <biddan606@gmail.com>
2025-01-14 00:08:24 +0900

    Springboot Project 초기화

---
+++ src/main/java/dev/biddan/nubblev2/NubbleV2Application.java
@@ -0,0 +1,13 @@
+package dev.biddan.nubblev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubbleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NubbleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/biddan/nubblev2/NubbleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.biddan.nubblev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubbleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
