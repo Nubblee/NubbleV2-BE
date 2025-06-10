3cd2c33dbd3762916c85ccd33e875120d71e51b8
biddan606 <biddan606@gmail.com>
2025-06-10 20:22:57 +0900

    유저 정보 조회 기능 구현

--- src/main/java/dev/biddan/nubblev2/user/controller/UserApiController.java
+++ src/main/java/dev/biddan/nubblev2/user/controller/UserApiController.java
@@ -1,25 +1,32 @@
 package dev.biddan.nubblev2.user.controller;
 
+import dev.biddan.nubblev2.argument.userid.CurrentUserId;
+import dev.biddan.nubblev2.interceptor.auth.AuthRequired;
+import dev.biddan.nubblev2.user.repository.UserRepository;
 import dev.biddan.nubblev2.user.service.UserService;
 import dev.biddan.nubblev2.user.service.dto.UserInfo;
+import dev.biddan.nubblev2.user.service.dto.UserInfo.Private;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/api/v1/users")
 public class UserApiController {
 
     private final UserService userService;
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            path = "/api/v1/users",
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<UserApiResponse.Private> register(@RequestBody @Valid UserApiRequest.Register request) {
 
         UserInfo.Private info = userService.register(request.toRegisterCommand());
@@ -28,5 +35,13 @@ public class UserApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @AuthRequired
+    @GetMapping(path = "/api/v1/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<UserApiResponse.Private> getUser(@CurrentUserId Long userId) {
+        Private userInfo = userService.getUserById(userId);
+
+        return  ResponseEntity.ok(new UserApiResponse.Private(userInfo));
     }
 }
--- src/main/java/dev/biddan/nubblev2/user/service/UserService.java
+++ src/main/java/dev/biddan/nubblev2/user/service/UserService.java
@@ -1,10 +1,13 @@
 package dev.biddan.nubblev2.user.service;
 
+import dev.biddan.nubblev2.exception.http.NotFoundException;
 import dev.biddan.nubblev2.user.domain.User;
+import dev.biddan.nubblev2.user.repository.UserRepository;
 import dev.biddan.nubblev2.user.service.dto.UserCommand;
 import dev.biddan.nubblev2.user.service.dto.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,7 @@ public class UserService {
     private final UserDuplicateValidator userDuplicateValidator;
     private final UserCreator userCreator;
     private final PasswordHasher passwordHasher;
+    private final UserRepository userRepository;
 
     public UserInfo.Private register(UserCommand.Register command) {
         userDuplicateValidator.validate(command.loginId(), command.nickname());
@@ -27,6 +31,14 @@ public class UserService {
                 .build();
 
         User user = userCreator.create(hashedPasswordCommand);
+
+        return UserInfo.Private.from(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserInfo.Private getUserById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("유저가 존재하지 않습니다"));
 
         return UserInfo.Private.from(user);
     }
